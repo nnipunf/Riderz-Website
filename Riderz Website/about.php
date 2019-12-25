d8306dfa0fcad0331d355021625c2c7d155e5e0a
nnipunf <41870880+nnipunf@users.noreply.github.com>
2019-12-25 14:00:39 +0600

    Add files via upload

---
+++ Riderz Website/about.php
@@ -0,0 +1,90 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    
+    <title>Document</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+
+    </head>
+
+    <body>
+        
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">RIDERZ</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+
+
+
+        <li class="nav-item active">
+            <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="about.php">About</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+        </li>
+
+
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+    </nav>
+
+
+  <div class="jumbotron">
+    <h1>RIDERZ SOCIETY</h1>
+    <p>RIDING will always RULE</p>
+  </div>
+
+    <section class="my-5">
+    <div class="py-5">
+    <h2 class="text-center">About Us</h2>
+    </div>
+    <div class="container-fluid">
+
+    <div class="row">
+
+    <div class="col-lg-6 col-md-6 col-12">
+    <img src="images/p4.jpg" class="img-fluid aboutimg">
+    </div>
+
+    <div class="col-lg-6 col-md-6 col-12">
+    <h2 class="display-4">We are the ANARCHY</h2>
+    <p class="py-3">
+    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
+    </p>
+    <a href="about.php" class="btn btn-success">Wanna know more</a>
+
+    </div>
+
+    </div>
+    </div>
+
+
+    </section>
+
+    <footer>
+    
+    <p class="p-3 bg-dark text-white text-center">nipun.ferdous@g.bracu.ac.bd</p>
+    </footer>
+
+</body>
+</html>
